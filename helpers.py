6c204fdd2de9c9504b1fcbaf3e1762985829a831
allen lin <cgallen@pm.me>
2018-11-24 23:42:09 +0800

    fix some problem

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 import requests
 import urllib.parse
 
-from flask import redirect, render_template, request, session, make_response, set_cookie
+from flask import redirect, render_template, request, session, make_response
 from functools import wraps
 
 def login_required(f):
