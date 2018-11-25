24c57166d145e57f9989fb03cb0ac78d58e0292c
allen lin <cgallen@pm.me>
2018-11-25 15:26:45 +0800

    idk what im doing

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, g
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
@@ -10,6 +10,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required
 
 app = Flask(__name__)
+
 
 # Ensure responses aren't cached
 @app.after_request
@@ -26,8 +27,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = sqlite3.connect("quiz.db")
 
 @app.route('/')
 def welcome():
@@ -38,9 +37,13 @@ def welcome():
 def register():
     """Register user"""
 
+
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
+        con = sql.connect("quiz.db")
+        db = con.cursor()
+        
         # Ensure username was submitted
         if not request.form.get("username"):
             return apology("Missing USER NAME")
