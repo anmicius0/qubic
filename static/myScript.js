8b41bebe33a56990f8d7fce42ef92b52b156dc47
allen lin <cgallen@pm.me>
2018-12-15 14:50:16 +0800

    Only styling issue left

--- static/myScript.js
+++ static/myScript.js
@@ -1,19 +1,17 @@
-function checkanswer(the_id, the_answer){
+function checkanswer(the_id, the_answer, solution){
   var x = String(the_id);
   var y = String(the_answer);
 
   if (x == y){
-    document.getElementById("op_block").style.display = "none"
-    document.getElementById("correct_animation").style.display = "block";
-    document.getElementById("worng_animation").style.display = "none";
+    swal("Correct!", solution, "success");
+    document.getElementById("correct-record").submit();
   }
   else{
-    document.getElementById("op_block").style.display = "none"
-    document.getElementById("correct_animation").style.display = "none";
-    document.getElementById("worng_animation").style.display = "block";
+    swal("No no", solution, "error");
+    document.getElementById("wrong-record").submit();
   }
 }
 
 function submit(the_id){
-  document.getElementById(the_id).submit(); 
+  document.getElementById(the_id).submit();
 }
